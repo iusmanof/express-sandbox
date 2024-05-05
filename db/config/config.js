293e3e80981eb12516fa22745e738c453171506f
iusmanof <iusmanof@gmail.com>
2024-05-05 10:33:39 +0300

    feat: add dotenv

---
+++ db/config/config.js
@@ -0,0 +1,25 @@
+require('dotenv').config()
+module.exports = { 
+    development: {
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      host:  process.env.POSTGRES_HOST,
+      dialect: process.env.POSTGRES_DIALECT
+    }
+  //   "test": {
+  //     "username": "root",
+  //     "password": null,
+  //     "database": "database_test",
+  //     "host": "127.0.0.1",
+  //     "dialect": "mysql"
+  //   },
+  //   "production": {
+  //     "username": "root",
+  //     "password": null,
+  //     "database": "database_production",
+  //     "host": "127.0.0.1",
+  //     "dialect": "mysql"
+  //   }
+  // }
+}
--- db/models/index.js
+++ db/models/index.js
@@ -3,12 +3,13 @@
 const Sequelize = require('sequelize');
 const process = require('process');
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+const config = require(__dirname + '/../config/config.js')[env];
 const db = {};
 const book = require('./books');
 
 let sequelize;
 if (config.use_env_variable) {
+  console.log(config)
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
