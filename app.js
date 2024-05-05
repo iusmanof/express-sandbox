9d1ccc798b77047260e732b14a0ff37a486fbc63
iusmanof <iusmanof@gmail.com>
2024-05-05 10:33:15 +0300

    fix: app.js

--- app.js
+++ app.js
@@ -5,11 +5,7 @@ const app = express();
 // middleware
 app.use(express.json());
 
-//difine all our routes
 app.use("/", bookRoutes);
-// app.get("/", (req, res) => {
-//   res.send("Hello world");
-// });
 
 const port = 3000;
 app.listen(port, () => {
