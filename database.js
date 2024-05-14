8fdc139be04f6e431eb64a235883a170f49f822f
iusmanof <iusmanof@gmail.com>
2024-05-13 23:12:42 -0700

    fix: database

--- database.js
+++ database.js
@@ -1,7 +1,7 @@
 const { Pool } = require('pg')
  
 const pool = new Pool({
-  connectionString: process.env.POSTGRES_URL,
+  connectionString: process.env.POSTGRES_URL + "?sslmode=require",
 })
 
 pool.connect((err) => {
